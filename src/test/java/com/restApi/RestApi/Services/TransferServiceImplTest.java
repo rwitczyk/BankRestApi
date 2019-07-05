f93769f4d0dbd2d66fcaef6c1399d85060def4bb
rwitczyk <r.witczyk@wp.pl>
2019-07-05 09:46:30 -0400

    Added simple test for test CI

---
+++ src/test/java/com/restApi/RestApi/Services/TransferServiceImplTest.java
@@ -0,0 +1,13 @@
+package com.restApi.RestApi.Services;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TransferServiceImplTest {
+
+    @Test
+    public void convertCurrency() {
+        int expected = 4;
+        Assert.assertEquals(expected,2*2);
+    }
+}
