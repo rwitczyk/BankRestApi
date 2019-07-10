94e9d63cefe29fbd042edccee2341ea91e85a5a8
rwitczyk <r.witczyk@wp.pl>
2019-07-10 09:53:18 -0400

    Small fixes

--- src/main/java/com/restApi/RestApi/Endpoints/AccountEndpoint.java
+++ src/main/java/com/restApi/RestApi/Endpoints/AccountEndpoint.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api")
 public class AccountEndpoint {
